d1730ef3c507e13b79ad223beaca4013d67f83d3
João Felipe <jfss2@cin.ufpe.br>
2020-06-03 21:09:28 -0300

    terceiro commit - terceiro dia

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -12,7 +12,6 @@ function populateUFs () {
     } )
 }
 populateUFs()
-
 function getCities(event) {
     const citySelect = document.querySelector("select[name=city]")
     const stateInput = document.querySelector("input[name=state]")
@@ -25,20 +24,59 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
     fetch(url)
     .then( res => res.json() )
     .then (cities => {
+        
         for ( const city of cities ) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
         
         citySelect.disabled = false
     })
 
 }
-
-
-
 document
     .querySelector('select[name=uf]')
     .addEventListener("change", getCities)
+
+const itemsToCollect = document.querySelectorAll("#items-grid li")
+
+for (let item of itemsToCollect) {
+    item.addEventListener("click", handleSelectItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectItem() {
+
+    const itemLi = event.target
+
+    //adicionar ou tirar uma classe c javascript
+    itemLi.classList.toggle('selected') //toggle consegue saber qnd o elemento tem a classe ou não
+
+    const itemId = itemLi.dataset.id
+    
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId
+        return itemFound
+    } )
+
+    if (alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter( item => {
+            const ItemIsDifferent = item != itemId
+            return false
+        })
+
+        selectedItems = filteredItems
+    } else {
+        selectedItems.push(itemId)
+    }
+
+    collectedItems.value = selectedItems
+}
---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+const buttonSearch = document.querySelector("#page-home main a")
+
+const modal = document.querySelector("#modal")
+buttonSearch.addEventListener("click", () => {
+    modal.classList.toggle('hide')
+})

3d2f3f7bd1f1eb787c721716ae635c713fadd2fa
João Felipe <jfss2@cin.ufpe.br>
2020-06-04 16:43:18 -0300

    melhora na responsividade

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
-const buttonSearch = document.querySelector("#page-home main a")
-
-const modal = document.querySelector("#modal")
-buttonSearch.addEventListener("click", () => {
+function openModal() {
+    const buttonSearch = document.querySelector("#page-home main a")
+    const modal = document.querySelector("#modal")
     modal.classList.toggle('hide')
-})
+}
+
